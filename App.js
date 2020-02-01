9d1f79b9f5c4553d3d0fac5aeb06b1f56026f67e
pstoebenau <pstoebenau@knights.ucf.edu>
2020-02-01 15:36:36 -0500

    Added skeleton of app UI

--- App.js
+++ App.js
@@ -1,62 +1,40 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Button, Alert, Image } from 'react-native'
 
-import AppNavigator from './navigation/AppNavigator';
-
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
+export default class HelloWorldApp extends Component {
+  render() {
     return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
+      <View style={styles.body}>
+        <View style={styles.topBar}>
+          <Button
+            title="Start trail"
+            onPress={() => Alert.alert('Trail started')}
+          />
+          <Button
+            title="End trail"
+            onPress={() => Alert.alert('Trail started')}
+          />
+        </View>
+
+        <Image
+        style={{width: 400, height: 600}}
+        source={{uri: 'https://facebook.github.io/react-native/img/tiny_logo.png'}}
+        />
       </View>
     );
   }
 }
 
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
-
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
   },
+
+  topBar: {
+    height: 100,
+    flexDirection: 'row',
+    paddingTop: 40,
+    justifyContent: 'space-between',
+  }
 });
